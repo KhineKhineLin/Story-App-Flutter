36e938b13da3910f790d23a275be06a226a99e8c
KhineKhineLin <santun2811@gmail.com>
2020-12-27 15:12:05 +0700

    story app

---
+++ lib/data.dart
@@ -0,0 +1,7 @@
+List<String> images = [
+  'assets/1.jpg',
+  'assets/2.jpeg',
+  'assets/3.png',
+  'assets/4.jpg'
+];
+List<String> title = ['Cindrella', 'Snow White', 'Aladdin', 'Sleeping Beauty'];
